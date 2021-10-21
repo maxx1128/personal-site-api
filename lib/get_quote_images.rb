fe990fe3ecbe217fef7dabb732f17602ae681cef
Max Antonucci <maxx1128@gmail.com>
2021-10-21 15:02:57 -0400

    move quote images to external repo

--- lib/get_quote_images.rb
+++ lib/get_quote_images.rb
@@ -1,19 +1,29 @@
+require 'net/http'
+require 'json'
+
 class GetQuoteImages
-  def initialize
-    @image_paths = Dir.glob("images/quotes/*.{png,jpeg}")
-  end
+  GITHUB_URL = 'https://api.github.com/repos/maxx1128/anime-quote-collection/git/trees/master?recursive=1'
 
   def get_random_image
     get_github_raw_image_url(random_image_path)
   end
 
-  private
-
-  def random_image_path
-    @image_paths.sample
+  def get_github_raw_image_url(path)
+    "https://raw.githubusercontent.com/maxx1128/anime-quote-collection/master/#{path}"
   end
 
-  def get_github_raw_image_url(path)
-    "https://raw.githubusercontent.com/maxx1128/personal-site-api/master/#{path}"
+  def random_image_path
+    all_images_paths.sample
+  end
+
+  def all_images_paths
+    github_api_data["tree"]
+      .select { |item| item["path"].include?("images/") }
+      .map { |item| item["path"] }
+  end
+
+  def github_api_data
+    uri = URI(GITHUB_URL)
+    JSON.parse(Net::HTTP.get(uri))
   end
 end
